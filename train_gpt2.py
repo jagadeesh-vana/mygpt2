ff3795fe07ea6b33dd81c48870cc2ad108c3371b
jagadeesh-vana <jagadeesh.vana@gmail.com>
2024-07-29 20:57:58 +0530

    GPT2 weights initialization

--- train_gpt2.py
+++ train_gpt2.py
@@ -27,6 +27,7 @@ class CausalSelfAttention(nn.Module):
         #A linear layer that projects the concatenated output of the attention heads back to the 
         # original embedding size.
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.MYGPT2_SCALE_INIT = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -75,6 +76,7 @@ class MLP(nn.Module):
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu    = nn.GELU(approximate='tanh')
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.MYGPT2_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -119,7 +121,22 @@ class GPT(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         
         # weight sharing scheme
-        self.transformer.wte.weight = self.lm_head.weight
+        self.transformer.wte.weight = self.lm_head.weight   
+        
+        # init params
+        self.apply(self.__init_weights)
+        
+    def __init_weights(self, module):        
+        
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'MYGPT2_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
         
     def forward(self, idx, targets=None):
         # idx is of shape (B, T)
